abbcd4894d7380b7fb343aafb2f8fa4b4354897a
ProbablyNotArtyom <notartyomowo@gmail.com>
2020-10-21 12:41:19 -0400

    various changes to the 128-lense effect

--- src/128-lense.asm
+++ src/128-lense.asm
@@ -7,12 +7,18 @@
 
 //-----------------------------------------------------
 
-.var Screen 			= $0400
-.var Color 				= $D800
+.const C64_COLOR_BG			= COLOR_BLUE
+.const C64_COLOR_BORDER		= COLOR_LIGHTBLUE
+.const C128_COLOR_BG		= COLOR_GRAY1
+.const C128_COLOR_BORDER	= COLOR_LIGHTGREEN
 
-.var rirq_count 		= $20
-.var _rirq_count 		= $21
-.var offset 			= $22
+.var Screen 	= $0400
+.var Color 		= $D800
+
+.var rirq_count 	= $20
+.var _rirq_count 	= $21
+.var offset 		= $22
+.var frames 		= $23
 
 //-------------------------------------------------------------------
 
@@ -24,48 +30,59 @@ setup:
 	sei
 	lda #$35
 	sta $01
-	
-	/* Just in case you're running a custom kernal...
-	we make sure you get the full experience ;) */
-	lda #COLOR_LIGHTBLUE
+
+	/* Just in case you're running a custom kernal... */
+	/* ...we make sure you get the full experience ;) */
+	lda #C64_COLOR_BORDER
 	sta VIC_border_color
-	lda #COLOR_BLUE
+	lda #C64_COLOR_BG
 	sta VIC_bg_color0
-	
+
 	ClearColorRam(COLOR_WHITE)
-	
+
 	:mov16 #irq0 : sysvec_IRQ
 	ldx #$00
-	stx $DC0E
+	stx CIA1_CRA
 	inx
 	stx VIC_irq_mask
-	lda #$1B
+
+	lda #%01111111
+	sta CIA2_ICR
+	and VIC_config1
 	sta VIC_config1
-	lda #$1
+
+	lda #$00
+	sta frames
+	lda #$02
 	sta VIC_raster
 	sta offset
 
-	lda #$01
+	lda #$C0
 	sta rirq_count
+
 	cli
 	jmp *
 
-irq0:
+irq1:
 	:irq_entry
-	:mov16 #irq1 : sysvec_IRQ
+	:mov16 #irq0 : sysvec_IRQ
 
-	lda #$08
-	ldx #COLOR_GRAY1
-	sta VIC_config2
+	ldx #C64_COLOR_BG
+	ldy #C64_COLOR_BORDER
+	lda VIC_raster
+!:	cmp VIC_raster
+	beq !-
+	sty VIC_border_color
 	stx VIC_bg_color0
-	dec VIC_border_color
+	lda #$08
+	sta VIC_config2
 
 	inc rirq_count
 	ldx rirq_count
 
 	lda sine256_Y,x
 	sta VIC_raster
-	
+
 	lda anti_sine256_Y,x
 	sta offset
 
@@ -73,13 +90,18 @@ irq0:
 	:irq_exit
 	rti
 
-irq1:
+irq0:
 	:irq_entry
-	inc VIC_border_color
-	lda #COLOR_BLUE
-	sta VIC_bg_color0
 
-	:mov16 #irq0 : sysvec_IRQ
+	ldx #C128_COLOR_BG
+	ldy #C128_COLOR_BORDER
+	lda VIC_raster
+!:	cmp VIC_raster
+	beq !-
+	sty VIC_border_color
+	stx VIC_bg_color0
+
+	:mov16 #irq1 : sysvec_IRQ
 
 	ldx offset
 !:	ldy VIC_raster
@@ -95,7 +117,8 @@ irq1:
 	dex
 	bne !--
 
-	lda #$1
+	lda VIC_raster
+	adc #$10
 	sta VIC_raster
 	:irq_exit
 	rti
@@ -109,6 +132,7 @@ sine256_X:
 sine256_Y:
 	.for (var i=0; i<256; i++)
 		.byte round(127.5+127.5*sin(toRadians(i*360/256)))
+
 anti_sine256_Y:
 	.for (var i=0; i<256; i++)
 		.byte round(127.5+120.5*sin(toRadians(i*360/256)))
@@ -116,6 +140,16 @@ anti_sine256_Y:
 modsine256_Y:
 	.for (var i=0; i<256; i++) {
 		.byte round(127.5+127.5 *
+			(
+				sin(toRadians(360*i/128)) *
+				cos(toRadians(360*i/64))
+			)
+		)
+	}
+
+modsine256_X:
+	.for (var i=0; i<256; i++) {
+		.byte round(3.5+3.5 *
 			(
 				sin(toRadians(360*i/128)) *
 				cos(toRadians(360*i/64))
